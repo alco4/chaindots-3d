90fa3899a7c66f80c95b0f08733cfc9eabeda4a4
Alko <alancorcos@hotmail.com>
2022-12-25 19:25:38 -0300

    removed unused elements

--- src/index.js
+++ src/index.js
@@ -5,34 +5,6 @@ import { Physics, usePlane, useBox } from "@react-three/cannon";
 import ImagePlane from "./ImagePlane";
 import "./styles.css";
 
-function Box() {
-  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
-  return (
-    <mesh
-      onClick={() => {
-        api.velocity.set(0, 2, 0);
-      }}
-      ref={ref}
-      position={[0, 2, 0]}
-    >
-      <boxBufferGeometry attach="geometry" />
-      <meshLambertMaterial attach="material" color="hotpink" />
-    </mesh>
-  );
-}
-
-function Plane() {
-  const [ref] = usePlane(() => ({
-    rotation: [-Math.PI / 2, 0, 0],
-  }));
-  return (
-    <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeBufferGeometry attach="geometry" args={[100, 100]} />
-      <meshLambertMaterial attach="material" color="lightblue" />
-    </mesh>
-  );
-}
-
 createRoot(document.getElementById("root")).render(
   <Canvas>
     <OrbitControls />
